3453f075c11464416121c22ad4d65bca3e20c3c7
Kavisha Chandraratne <kavishachandraratne@Kavishas-MacBook-Air.local>
2023-07-26 06:12:29 +1000

    a

--- basin.c
+++ basin.c
@@ -65,13 +65,13 @@ void fwrite_big_endian_64(FILE *fout, u_int64_t number, int byte_length) {
 int fread_next_256byte_block(FILE *fin, char block[]) {
     int result = 0;
     for (int j = 0; j < 256; j++) {
-        //printf("reading %dth char\n", j);
+        printf("reading %dth char\n", j);
         int c;
         if ((c = fgetc(fin)) == EOF ) {
-            //printf("char is EOF\n", j);
+            printf("char is EOF\n", j);
             return result;
         }
-        //printf("assigning block[%d] = %c\n", j, c);
+        printf("assigning block[%d] = %c\n", j, c);
         result++;
         block[j] = c;
     }
