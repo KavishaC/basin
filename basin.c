e9e31e47e843697d62f6008613e70c284b3f5f20
Kavisha Chandraratne <kavishachandraratne@Kavishas-MacBook-Air.local>
2023-07-28 06:55:07 +1000

    a

--- basin.c
+++ basin.c
@@ -623,8 +623,8 @@ void stage_3(char *out_filename, char *in_filename) {
             write_updates_to_file(ftcbi, pathname, updates, num_blocks);
         }
         int filesize = read_filesize_from_file(in_filename);
-   
-        //printf("num_blocks(%d) * 256 > filesize(%d)\n", num_blocks, filesize);
+
+        printf("num_blocks_indicated(%d) > num_blocks_read(%d)\n", num_blocks, number_of_blocks_in_file(filesize));
 /*         if (((num_blocks - 1) * BLOCK_SIZE) > filesize) {
             perror("num blocks too high for filesize");
             exit(1);
