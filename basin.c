90972240093640946efd066a024efaefab1f2992
Kavisha Chandraratne <kavishachandraratne@Kavishas-MacBook-Air.local>
2023-07-28 02:25:24 +1000

    a

--- basin.c
+++ basin.c
@@ -689,11 +689,11 @@ void stage_4(char *in_filename) {
         FILE *target = fopen(pathname, "r+");
         if (target == NULL) {
             target = fopen(pathname, "w");
-        printf("Came here successfully\n");
             if (target == NULL) {
                 perror(pathname);
                 exit(1);
             }
+        printf("Came here successfully\n");
         }
         mode_t new_mode = read_mode_from_tcbi_file(ftcbi);
         update_mode(pathname, new_mode);
