d74ef5a1ffdb3bfa206968627efcadd60fdcc273
Kavisha Chandraratne <kavishachandraratne@Kavishas-MacBook-Air.local>
2023-07-28 07:21:34 +1000

    a

--- basin.c
+++ basin.c
@@ -244,7 +244,7 @@ char *read_pathname(FILE *fin) {
     }; */
 
 
-    printf("pathname_length = %u\n", pathname_length);
+    //printf("pathname_length = %u\n", pathname_length);
 
     char *pathname = malloc(sizeof(char)*(pathname_length + 1));
     pathname[pathname_length] = '\0';
